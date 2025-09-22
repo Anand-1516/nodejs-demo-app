f09f0076e13e4855325f5519bb2c2296efaa25c4
Anand-1516 <banandakrishnan6@gmail.com>
2025-09-22 14:25:23 +0000

    updated one

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-  res.send('Hello this is my first task for CI/CD — updated code!');
-=======
   res.send('Hello world CI/CD — updated code!');
->>>>>>> 16c0256 (updated one)
 });
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
 });
-
