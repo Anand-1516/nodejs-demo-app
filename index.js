ead2f5f261c81cf0ad84aecb3f95bb2e7b545343
Anandakrishnan <banandakrishnan6@gmail.com>
2025-09-22 19:35:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD — updated code!');
+  res.send('Hello this is my first task for CI/CD — updated code!');
 });
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
 });
+
